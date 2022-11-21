fa37466539edc4dcb16017de137c1bd3998bfb2a
Programmer Hat <hello@programmerhat.com>
2022-11-21 23:45:10 +0000

    landing page for SEO

---
+++ index.php
@@ -0,0 +1,94 @@
+<title>Vim Online - Vim Editor In Browser</title>
+<h1>Vim Online Editor</h1>
+
+<p>
+  Got a feature request? We'd love to hear it! We know there's a lot of work to do to make Vim Online Editor as good as normal Vim. So send an email to <a href='mailto:hello@programmerhat.com'>hello@programmerhat.com</a>
+</p>
+
+<h2>What is the Vim Online Editor?</h2>
+
+<p>
+  This is "Vim Online", a vim editor in browser. You can install your vimrc, and this app will remember your vimrc between visits.
+</p>
+
+<p>
+  The Vim Online Editor is building off groundbreaking efforts by @rhysd and @coolwanglu to bring vim to the browser.
+</p>
+
+<p>
+  While those projects did a great job getting vim functionality inside the browser, there are still many missing pieces. The most important missing feature in my opinion is being able to install a vimrc to get all the normal keybindings back.
+</p>
+
+<p>
+  Another really important missing feature is being able to save files easily and navigate between files easily.
+</p>
+
+<p>
+  Another really important feature is being able to git clone a repo into the browser.
+</p>
+
+<p>
+  What would really be cool is being able to edit files from the filesystem, using the WASI API.
+</p>
+
+<p>
+  Even if direct access to the filesystem isn't possible, an autosync with the source code so that you could easily test the code would be super cool.
+</p>
+
+<h2>Who made the Vim Online Editor?</h2>
+
+<p>
+  "Vim Online" was built by the lovely folks at <a href="https://www.programmerhat.com">programmerhat.com</a>.
+</p>
+
+<p>
+  We happen to be huge Vim enthusiasts ourselves. Using Vim for many years. It's the best editor in the world.
+</p>
+
+<p>
+  And of course we're software engineers! We like building software.
+</p>
+
+<p>
+  What we'd LOVE to hear from you are FEATURE REQUESTS! We know there's a lot of work needed to make this as good as the Vim you're used to in the terminal. So send an email to <a href='mailto:hello@programmerhat.com'>hello@programmerhat.com</a>
+</p>
+
+<h2>How to use Vim Online Editor?</h2>
+
+<p>
+  If you've got your own vimrc, you'll probably want to install that straight away. Go to "Upload vimrc" so that you get all the keybindings you're used to.
+</p>
+
+<p>
+  Caveat is that this app currently does not support plugins.
+</p>
+
+<p>
+  Then just click the vim box and you're good to go!
+</p>
+
+<p>
+  It's free. Doesn't cost anything. There will be some ads to help fund feature development. There are a lot of features I want to build.
+</p>
+
+<h2>Why use Vim Online Editor?</h2>
+
+<ol>
+  <li>Because you love vim.</li>
+  <li>Because you don't have access to vim somehow (maybe you're on a Chromebook that doesn't allow access to the system)</li>
+  <li>Because you want an notepad of some sort in the browser, and you want to use vim bindings instead of normal notepad.</li>
+<ol>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-25460549-35', 'auto');
+  ga('send', 'pageview');
+</script>
+<!-- End Google Analytics -->
